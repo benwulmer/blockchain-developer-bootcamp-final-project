ecc24f5d377c6ada448b23b077cf5a63facf5bab
Ben Ulmer <ben.ulmer@coinbase.com>
2021-11-23 17:33:23 -0800

    Updates

---
+++ precache-manifest.ae8a61521e480b58d5445668d6316aa5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13025c10aadbe2e319a05d113e0bed32",
+    "url": "/blockchain-developer-bootcamp-final-project/index.html"
+  },
+  {
+    "revision": "16e658bb1353aa9fbd90",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3a2d4a1a9fa609930c8c",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/2.16e8a86a.chunk.js"
+  },
+  {
+    "revision": "16e658bb1353aa9fbd90",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/main.8bf89c5b.chunk.js"
+  },
+  {
+    "revision": "974fe9a407c487a617cb",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/runtime-main.dca84915.js"
+  }
+]);
